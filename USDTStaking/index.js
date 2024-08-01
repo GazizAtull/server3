3189b4e1909f90346aa6533522c62cafde3e1204
GazizAtull <138782569+GazizAtull@users.noreply.github.com>
2024-08-02 01:46:53 +0500

    Update index.js

--- USDTStaking/index.js
+++ USDTStaking/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const index = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 index.use(bodyParser.json());
 index.use(cors());
